33ca5075cec47e79fb37b517e8faf4091f4a6b0e
Alex Holmes <alexander.holmes@gmail.com>
2017-09-05 10:46:51 -0300

    add fullpage js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(function() {
+
+	// $('a').click(function(){
+	//     var target = $(this).attr('href');
+	//     if (target == '#')
+	//       $('html, body').animate({scrollTop : 0}, 600);
+	//     else
+	//       $('html, body').animate({
+	//         scrollTop: $(target).offset().top - 100
+	//     }, 600);
+	// });
+
+	// $(window).resize(function() {
+ //        // This will fire each time the window is resized:
+ //        if($(window).width() <= 468) {
+ //            // if larger or equal
+ //            $('.menu-full').hide();
+ //            $('.menu-mobile').show();
+ //        } else {
+ //            $('.menu-mobile').hide();
+ //        }
+ //    }).resize(); // This will simulate a resize to trigger the initial run.
+
+});
